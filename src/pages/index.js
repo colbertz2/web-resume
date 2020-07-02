e7130afbd718ab1a0e1dc2fed48f7759566ad75b
Zach Colbert <colbertz@oregonstate.edu>
2020-07-02 16:57:58 -0700

    Base content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <div>
+    <h1>Zach Colbert</h1>
+    <div>Zach is a guy looking for a job.</div>
+  </div>
+)
