84f4d4ea7e9b2f67f54cb659af0579e906739b33
Zach Colbert <zach.colbert20@gmail.com>
2020-07-02 17:09:52 -0700

    Install typography plugin and ocean beach theme

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import typoTheme from "typography-theme-ocean-beach"
+
+const typography = new Typography(typoTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
