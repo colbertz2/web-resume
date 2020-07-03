fbfe5115d7863fc7830cfb5dc2674eda68f3cc35
Zach Colbert <zach.colbert20@gmail.com>
2020-07-02 21:00:01 -0700

    Add header image

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,14 +4,33 @@ import { useStaticQuery, Link, graphql } from "gatsby"
 import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
 import { rhythm } from "../utils/typography"
+import headerImg from "../img/zach.jpg"
 
 const Header = styled.div`
   text-align: center;
   margin-bottom: ${rhythm(1)};
 `
 
+const HeaderImg = props => (
+  <div>
+    <img
+      src={props.src}
+      alt={props.alt}
+      css={css`
+        border-radius: 50%;
+        min-width: ${rhythm(5)};
+        max-width: 30%;
+        border: 2px solid black;
+        margin-top: ${rhythm(0.6)};
+        margin-bottom: 0;
+      `}
+    />
+  </div>
+)
+
 const Title = styled.h1`
-  margin-bottom: ${rhythm(0.5)};
+  margin: ${rhythm(0.5)};
+  // margin-bottom: ${rhythm(0.5)};
 `
 
 const Tagline = styled.em`
@@ -45,6 +64,7 @@ export default ({ children }) => {
         {/* Site Header */}
         <Header>
           <Link to={"/"}>
+            <HeaderImg src={headerImg} alt="zach.jpg" />
             <Title>{data.site.siteMetadata.author}</Title>
           </Link>
           <Tagline>Zach is a guy looking for a job.</Tagline>
