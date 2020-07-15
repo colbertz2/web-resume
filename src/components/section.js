54caf7505a4df09cd73c9b6a152f9a9015327e5d
Zach Colbert <zach.colbert20@gmail.com>
2020-07-14 20:50:39 -0700

    Underline links on hover only when they're provided a target

--- src/components/section.js
+++ src/components/section.js
@@ -2,12 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
-import { rhythm } from "../utils/typography"
+import { options, rhythm } from "../utils/typography"
 import Pad from "./pad"
+
+const hover = css`
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: ${options.headerColor};
+  }
+`
 
 const Section = props => (
   <div>
-    <Link to={props.link}>
+    <Link to={props.link} css={props.link ? hover : null}>
       <h2>{props.title}</h2>
     </Link>
     <Pad>{props.children}</Pad>
@@ -32,7 +39,7 @@ const Subsection = props => {
           margin-bottom: ${rhythm(0.5)};
         `}
       >
-        <Link to={props.link}>
+        <Link to={props.link} css={props.link ? hover : null}>
           <SubHeader>{props.title}</SubHeader>
         </Link>
         <em>{props.subtitle}</em>
