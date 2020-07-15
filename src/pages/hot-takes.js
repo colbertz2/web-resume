40c81efafa3ac6b5869eb64d9c9228ac25a44722
Zach Colbert <zach.colbert20@gmail.com>
2020-07-15 09:54:23 -0700

    Update takes and add footnotes

--- src/pages/hot-takes.js
+++ src/pages/hot-takes.js
@@ -7,17 +7,29 @@ import HotTakes from "../components/hot"
 var ht = {}
 ht["Editor"] = "Vim"
 ht["GUI Editor"] = "VS Code *"
-ht["Operating System"] = "MacOS Mojave or earlier"
-ht["Operating System (no, really)"] = "Manjaro Linux"
-ht["Phone"] = "iPhone (sorry)"
+ht["Operating System"] = "Manjaro Linux"
+// ht["Operating System (no, really)"] = "Manjaro Linux"
+ht["Phone"] = "iPhone (sorry, not sorry)"
 ht["VCS"] = "Git"
-ht["Programming Language"] = "C"
+ht["Programming Language"] = "C (and Python too!)"
 ht["Shell Language"] = "Zsh **"
 
 export default () => (
   <Layout>
     <Section title="Hot Takes">
       <HotTakes table={ht} />
+      <p>
+        <em>
+          * Open-source build, unless I need to use the Remote-SSH extension.
+          For some reason I can only get it to work with the Microsoft build.
+        </em>
+      </p>
+      <p>
+        <em>
+          ** With lovely, lovely configuration by{" "}
+          <a href="https://ohmyz.sh/">Oh My Zsh</a>!
+        </em>
+      </p>
     </Section>
   </Layout>
 )
