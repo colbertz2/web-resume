aacacc565b67e3f422e0a1565987f41f9852f8ea
Zach Colbert <zach.colbert20@gmail.com>
2020-07-16 14:13:30 -0700

    Change link color to blue

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,11 @@ import Typography from "typography"
 import typoTheme from "typography-theme-ocean-beach"
 
 typoTheme.baseFontSize = "14pt"
+typoTheme.overrideThemeStyles = () => ({
+  a: {
+    color: "rgb(65, 131, 196)",
+  },
+})
 const typography = new Typography(typoTheme)
 
 export const { scale, rhythm, options } = typography
