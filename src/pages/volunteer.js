ff2e4efedd964a9b5e0ceac2ff08b8b79aa541e3
Zach Colbert <zach.colbert20@gmail.com>
2020-07-21 00:34:43 -0700

    Summarize OA experience

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Section } from "../components/section"
 import Work from "../components/work"
+import { List, Item } from "../components/list"
 
 export default () => (
   <Layout>
@@ -13,7 +14,42 @@ export default () => (
         start="Oct 2018"
         end="Present"
       >
-        {/* <p>Description</p> */}
+        <List>
+          <Item>
+            Advise youth leaders on administrative, financial, and technology
+            needs of the organization.
+          </Item>
+          <Item>
+            Guide youth leaders through annual budget planning and approval
+            sessions.
+          </Item>
+          <Item>
+            Manage online registration service for five or more events of 85+
+            per year. Supervise staff at event check-in.
+          </Item>
+          <Item>
+            Administer organization websites using Wordpress and LAMP stack.
+          </Item>
+          <Item>Manage the organization's computer and printer resources.</Item>
+          <Item>
+            Administer access to member records system, cloud-based file
+            storage, websites, and social media accounts.
+          </Item>
+          <Item>
+            Track revenue and expenses throughout the year, and assist youth
+            leadership in reporting. Collect membership renewal and event
+            registration fees throughout the year, and process reimbursement
+            requests.
+          </Item>
+          <Item>
+            Coordinate with professional accountant to perform periodic audits
+            and manage the organization's assets.
+          </Item>
+          <Item>
+            Provide office support for membership renewal mailings, print and
+            electronic newsletters, fliers and handouts for events.
+          </Item>
+        </List>
       </Work>
 
       <Work
