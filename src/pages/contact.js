24ef448ca1730707c05480c7e401b90790cca64b
Zach Colbert <zach.colbert20@gmail.com>
2020-07-22 16:29:24 -0700

    Prettify contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,43 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <p>
-      <b>Email:</b> zach.colbert20 AT gmail.com
-    </p>
-    <p>
-      <b>GPG Key</b>: Coming soon
-    </p>
+    <table>
+      <tr>
+        <td>
+          <b>Email</b>
+        </td>
+        <td>zach.colbert20 AT gmail.com</td>
+      </tr>
+      <tr>
+        <td>
+          <b>GPG Key</b>
+        </td>
+        <td>Coming soon</td>
+      </tr>
+      <tr>
+        <td>
+          <b>Twitter</b>
+        </td>
+        <td>
+          <a href="https://twitter.com/zach_colbert">@zach_colbert</a>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <b>LinkedIn</b>
+        </td>
+        <td>
+          <a href="https://www.linkedin.com/in/zach-colbert">zach-colbert</a>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <b>GitHub</b>
+        </td>
+        <td>
+          <a href="https://github.com/colbertz2">colbertz2</a>
+        </td>
+      </tr>
+    </table>
   </Layout>
 )
