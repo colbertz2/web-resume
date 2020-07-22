2c1a46f27df070721ffc9f10ed413c2366ea9430
Zach Colbert <zach.colbert20@gmail.com>
2020-07-21 17:23:28 -0700

    Fill out work experience section

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Section } from "../components/section"
 import Work from "../components/work"
+import { List, Item } from "../components/list"
 
 export default () => (
   <Layout>
@@ -13,7 +14,24 @@ export default () => (
         start="Nov 2016"
         end="July 2020"
       >
-        {/* <p>Description</p> */}
+        <List>
+          <Item>
+            Provide high-quality remote, walk-up, and on-site desktop support
+            for an enterprise customer base.
+          </Item>
+          <Item>
+            Mentor student staff by providing actionable feedback and teaching
+            skills. Contribute articles to in-house knowledge base.
+          </Item>
+          <Item>
+            Manage directory and email services with Active Directory and
+            Microsoft Exchange 2010.
+          </Item>
+          <Item>
+            Manage macOS endpoints with Jamf Pro. Assist with building software
+            packages and configuration profiles.
+          </Item>
+        </List>
       </Work>
 
       <Work
@@ -22,7 +40,22 @@ export default () => (
         start="Jun 2019"
         end="Present"
       >
-        {/* <p>Description</p> */}
+        <List>
+          <Item>Interview, hire, and onboard seasonal staff members.</Item>
+          <Item>
+            Conduct one week of staff training and camp setup, including
+            sessions on effective teaching and learning, crisis management, and
+            child abuse prevention.
+          </Item>
+          <Item>
+            Provide leadership to a team of 35 staff ranging in age from 14 to
+            75 years old.
+          </Item>
+          <Item>
+            Set policies to ensure camp-wide compliance with National Camp
+            Assessment Program standards set by the BSA.
+          </Item>
+        </List>
       </Work>
 
       <Work
@@ -31,7 +64,19 @@ export default () => (
         start="Jun 2018"
         end="Sep 2018"
       >
-        {/* <p>Description</p> */}
+        <List>
+          <Item>Set research goals for senior thesis project.</Item>
+          <Item>
+            Review literature, obtain supplies and equipment in support of
+            research goals.
+          </Item>
+          <Item>Maintain optical equipment and clean shared lab spaces.</Item>
+          <Item>
+            Report results at weekly group meetings. Perform other duties as
+            necessary to support the research goals of the group and other
+            researchers.
+          </Item>
+        </List>
       </Work>
 
       <Work
@@ -40,7 +85,20 @@ export default () => (
         start="Nov 2014"
         end="Jun 2020"
       >
-        {/* <p>Description</p> */}
+        <List>
+          <Item>
+            Provide excellent customer service by guiding consumers to the
+            products they are looking for.
+          </Item>
+          <Item>
+            Maintain in-depth knowledge of core and seasonal products.
+          </Item>
+          <Item>
+            Ensure the sales floor is well-stocked, and process incoming
+            freight.
+          </Item>
+          <Item>Clean workspaces and the sales floor.</Item>
+        </List>
       </Work>
     </Section>
   </Layout>
