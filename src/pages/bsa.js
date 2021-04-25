a3c4b9c701dd6b77d80edc5f7b1d009e246e43c5
Zach Colbert <zach.colbert20@gmail.com>
2021-04-24 21:30:10 -0700

    Add contact page for Programming MB

---
+++ src/pages/bsa.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <table>
+      <tr>
+        <td>
+          <b>Programming MB Resources</b>
+        </td>
+        <td>
+          <a href="https://github.com/colbertz2/Programming-Merit-Badge">
+          https://github.com/colbertz2/Programming-Merit-Badge
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <b>Email</b>
+        </td>
+        <td>
+          zcolbert AT sectionw1s.org
+        </td>
+      </tr>
+    </table>
+  </Layout>
+)
