46e3e9bff1060e7f32a610fb4cb7e8c3430e1cab
Zach Colbert <zach.colbert20@gmail.com>
2023-04-29 17:57:18 -0700

    Hide awards and volunteer work, update work experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,36 +3,22 @@ import React from "react"
 import Layout from "../components/layout"
 import { Section, Subsection } from "../components/section"
 import Work from "../components/work"
-import Award from "../components/award"
 
 export default () => (
   <Layout>
-    {/* <p>
-      Personal statement or intro paragraph. Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit. Vivamus non metus et felis ornare
-      sollicitudin a quis sem. Sed sed eros et nulla pulvinar euismod eu eget
-      erat. Duis nec nulla lacus. Etiam quis porta neque. Maecenas ex urna,
-      mollis eu vehicula sed, tincidunt ac justo. Nam commodo et risus eu
-      vestibulum. Quisque cursus nunc urna. Mauris eleifend efficitur tellus.
-      Sed nec porta mi. Vivamus mollis sapien quis tortor dignissim, non varius
-      turpis vehicula. Curabitur eu nisl cursus, placerat nulla imperdiet,
-      ultrices enim. Vivamus sit amet bibendum metus, mollis pretium nunc. Nulla
-      dignissim semper lorem sit amet facilisis. Suspendisse ut augue lacus.
-      Etiam tincidunt nisl est, ut commodo ante egestas nec.
-    </p> */}
-
-    <Section title="Education" link="/education">
-      <Subsection title="Oregon State University" aside="Corvallis, OR">
-        <p>Bachelor of Science in Physics, awarded June 2020.</p>
-      </Subsection>
-    </Section>
-
     <Section title="Work Experience" link="/work">
+      <Work
+        title="Desktop Technician"
+        company="Sigma Design, Inc."
+        start="Sept 2021"
+        end="Present"
+      />
+
       <Work
         title="IT Technician"
         company="TEC Equipment"
         start="April 2021"
-        end="Present"
+        end="Sept 2021"
       />
 
       <Work
@@ -53,43 +39,18 @@ export default () => (
         title="Resident Camp Director"
         company="Oregon Trail Council, Boy Scouts of America"
         start="Jun 2019"
-        end="Present"
+        end="Feb 2021"
       />
     </Section>
 
-    <Section title="Volunteer Work" link="/volunteer">
-      <Work
-        title="Administrative Adviser (Order of the Arrow)"
-        company="Oregon Trail Council, Boy Scouts of America"
-        start="Oct 2018"
-        end="Present"
-      />
-
-      <Work
-        title="Assistant Scoutmaster"
-        company="Boy Scouts of America"
-        start="Apr 2015"
-        end="Present"
-      />
-
-      <Work
-        title="Seasonal Camp Staff"
-        company="Oregon Trail Council, Boy Scouts of America"
-        start="Jul 2013"
-        end="Jul 2019"
-      />
+    <Section title="Education" link="/education">
+      <Subsection title="Oregon State University" aside="Corvallis, OR">
+        <p>Bachelor of Science in Physics, awarded June 2020.</p>
+      </Subsection>
     </Section>
 
-    <Section title="Awards and Honors">
-      <Award name="Eagle Scout" date="Mar 2015" org="Boy Scouts of America" />
+    <Section title="Volunteer Work" link="/volunteer"></Section>
 
-      <Award name="Vigil Honor" date="Jun 2017" org="Order of the Arrow, BSA" />
-
-      <Award
-        name="Founder's Award"
-        date="Oct 2016"
-        org="Order of the Arrow, BSA"
-      />
-    </Section>
+    <Section title="Awards and Honors" link="/awards"></Section>
   </Layout>
 )
